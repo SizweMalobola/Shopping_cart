50ee30f4954fd5445a13482d1d3d1b1a636e7a24
SizweMalobola <25reyals@gmail.com>
2021-06-09 23:31:55 +0200

    cleaned up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,65 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./navbarStyles.css";
-export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      homeNav: true,
-      shopNav: false,
-      aboutNav: false,
-    };
-  }
 
-  render() {
-    return (
-      <>
-        <nav id="navbar">
-          <div id="logo-div">
-            {/* <img
-              src="https://api.freelogodesign.org/files/14ce6bd6710045fd9be20e56c23292f3/thumb/logo_200x200.png?v=637540055620000000"
-              alt="little shop of horrors Logo"
-            ></img> */}
-          </div>
+function Navbar() {
+  const [homeNav, setHomeNav] = useState(false);
+  const [shopNav, setShopNav] = useState(false);
+  const [aboutNav, setAboutNav] = useState(false);
+  // Todo figure out why doesn't pathname/history update when I click to different links
+  const history = useHistory();
+  useEffect(() => {
+    let pathname = history.location.pathname;
+    // switch (pathname) {
+    //   case "/Shopping_cart":
+    //     setHomeNav(true);
+    //     setShopNav(false);
+    //     setAboutNav(false);
+    //     break;
+    //   case "/about":
+    //     setHomeNav(false);
+    //     setShopNav(false);
+    //     setAboutNav(true);
+    //     break;
+    //   default:
+    //     setHomeNav(false);
+    //     setShopNav(true);
+    //     setAboutNav(false);
+    //     break;
+    // }
+    // console.log(homeNav, shopNav, aboutNav);
+    console.log(pathname);
+  }, [history]);
 
-          <div id="nav-links-div">
-            <Link
-              to="/Shopping_cart"
-              className={this.state.homeNav ? "active-nav-link" : null}
-              onClick={() => {
-                this.setState({
-                  homeNav: true,
-                  shopNav: false,
-                  aboutNav: false,
-                });
-              }}
-            >
-              HOME
-            </Link>
+  return (
+    <>
+      <nav id="navbar">
+        <div id="logo-div">
+          <img
+            src="https://api.freelogodesign.org/files/14ce6bd6710045fd9be20e56c23292f3/thumb/logo_200x200.png?v=637540055620000000"
+            alt="little shop of horrors Logo"
+          ></img>
+        </div>
 
-            <Link
-              to="/shop"
-              className={this.state.shopNav ? "active-nav-link" : null}
-              onClick={() => {
-                this.setState({
-                  shopNav: true,
-                  homeNav: false,
-                  aboutNav: false,
-                });
-              }}
-            >
-              SHOP
-            </Link>
+        <div id="nav-links-div">
+          <Link
+            to="/Shopping_cart"
+            className={homeNav ? "active-nav-link" : null}
+          >
+            HOME
+          </Link>
 
-            <Link
-              to="/about"
-              className={this.state.aboutNav ? "active-nav-link" : null}
-              onClick={() => {
-                this.setState({
-                  aboutNav: true,
-                  homeNav: false,
-                  shopNav: false,
-                });
-              }}
-            >
-              ABOUT
-            </Link>
-          </div>
-        </nav>
-      </>
-    );
-  }
+          <Link to="/shop" className={shopNav ? "active-nav-link" : null}>
+            SHOP
+          </Link>
+
+          <Link to="/about" className={aboutNav ? "active-nav-link" : null}>
+            ABOUT
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
+
+export default Navbar;
