ff96e99b476befcaabb7691a0589a7f090893c53
SizweMalobola <25reyals@gmail.com>
2021-06-09 23:30:26 +0200

    cleaned up shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -233,7 +233,6 @@ export default class Shop extends Component {
   render() {
     return (
       <div id="shop-div">
-        <h1 className="header">Shop here</h1>
         <Cart
           removeItem={this.removeCartItem}
           cartItems={this.state.cartArray}
