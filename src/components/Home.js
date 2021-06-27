404d6be56d96c38a310392e09074d0deba53e75b
SizweMalobola <25reyals@gmail.com>
2021-06-27 16:50:35 +0200

    home page @ 320-400px is functional

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
-import "./homeStyles.css";
+import styles from "./homeStyle.module.css";
+import { Link } from "react-router-dom";
+
 export default class Home extends Component {
   render() {
     return (
-      <div id="home-div">
-        <h1>Welcome Home</h1>
+      <div className={styles["home-div"]}>
+        <div className={styles["hero"]}>
+          <span>WELCOME TO THE...</span>
+          <h1>Little shop Of Horrors</h1>
+        </div>
+        <img
+          className={styles["head-img"]}
+          src={process.env.PUBLIC_URL + "/assets/head.png"}
+          alt="head logo thing"
+        ></img>
+        <div className={styles["cta"]}>
+          <p>
+            If our collection of films can't quench your thrist for the
+            horrific, nothing will.
+          </p>
+          <Link to="/shop">Enter Shop</Link>
+        </div>
       </div>
     );
   }
