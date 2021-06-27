2a68b5b4c99b12aa946f8138af41092495b00be0
SizweMalobola <25reyals@gmail.com>
2021-06-27 13:47:11 +0200

    added css module for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,21 @@
-import React, { useEffect } from "react";
-import { NavLink, useHistory } from "react-router-dom";
-import "./navbarStyles.css";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./navbarStyle.module.css";
 
 function Navbar() {
-  const history = useHistory();
-  useEffect(() => {
-    let pathname = history.location.pathname;
-    console.log(pathname);
-  }, [history]);
-
   return (
     <>
-      <nav id="navbar">
-        <div id="logo-div">
-          <img
-            src="https://api.freelogodesign.org/files/14ce6bd6710045fd9be20e56c23292f3/thumb/logo_200x200.png?v=637540055620000000"
-            alt="little shop of horrors Logo"
-          ></img>
-        </div>
-
-        <div id="nav-links-div">
-          <NavLink to="/" exact activeClassName="active-nav-link">
+      <nav className={styles["navbar"]}>
+        <div className={styles["nav-links-div"]}>
+          <NavLink to="/" exact activeClassName={styles["active-nav-link"]}>
             HOME
           </NavLink>
 
-          <NavLink to="/shop" activeClassName="active-nav-link">
+          <NavLink to="/shop" activeClassName={styles["active-nav-link"]}>
             SHOP
           </NavLink>
 
-          <NavLink to="/about" activeClassName="active-nav-link">
+          <NavLink to="/about" activeClassName={styles["active-nav-link"]}>
             ABOUT
           </NavLink>
         </div>
