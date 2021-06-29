163a5bc6a41332b65ff047ef43db8c848794c112
SizweMalobola <25reyals@gmail.com>
2021-06-29 19:30:48 +0200

    finished building and styling about page @320px

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import "./aboutStyles.css";
+import styles from "./aboutStyle.module.css";
 export default class About extends Component {
   render() {
     return (
-      <div id="about-div">
-        <h1>About Us</h1>
+      <div className={styles["container"]}>
+        <h1>
+          <span>THE</span> ONLY PLACE <span>WHERE</span> YOU CAN{" "}
+          <span>HAVE</span> YOUR FILL <span>FOR</span> ALL THINGS{" "}
+          <span>HORROR</span> FROM <span>FOREIGN</span> TO{" "}
+          <span>COSMIC HORROR.</span> WE'VE GOT IT <span>ALL,</span> EVEN THE
+          REALLY <span>F$CK*D</span> UP STUFF FOR ALL YOU <span>SICK</span>{" "}
+          F!CKS OUT THERE. IF IT AIN'T <span>ILLEGAL</span> WE'VE PROBABLY{" "}
+          <span>GOT</span> IT.
+        </h1>
+        <img
+          alt="cannibal holocaust poster"
+          src={`${process.env.PUBLIC_URL}/assets/mouth.png`}
+        />
       </div>
     );
   }
