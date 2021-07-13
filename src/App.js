3a7f90e243bdad7ef5dde168993587237bb35569
SizweMalobola <25reyals@gmail.com>
2021-07-13 16:13:28 +0200

    removed basename from package.json

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
   render() {
     return (
       <div id="container">
-        <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+        <BrowserRouter>
           <Navbar />
           <Switch>
             <Route path="/" exact component={Home}></Route>
